8d3657b73b97a14440487e9eca1c45b1e6274d4b
David LAMBOURDIERE <davidlambourdiere@gmail.com>
2021-01-17 04:11:37 +0100

    Initial Commit

---
+++ websockethub/src/main/java/memda/episen/websockethub/WebSocketHubApplication.java
@@ -0,0 +1,13 @@
+package memda.episen.websockethub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebSocketHubApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebSocketHubApplication.class, args);
+    }
+
+}
---
+++ websockethub/src/test/java/memda/episen/websockethub/WebSocketHubApplicationTests.java
@@ -0,0 +1,13 @@
+package memda.episen.websockethub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebSocketHubApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
